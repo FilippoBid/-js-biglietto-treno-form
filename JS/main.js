45eb3d834553da6de9751f9f762e5368fafa585e
FilippoBid <filippoperrone1997@gmail.com>
2022-10-29 21:13:22 +0200

    metto la condizione if

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,7 @@
-const userKm = prompt("quanti Km vuoi percorrere?");
+/*const userKm = prompt("quanti Km vuoi percorrere?");
 
 const userAge = prompt("quanti anni hai?");
 
-/*const actualFullDate = new Date(); 
-
-const actualYear = actualFullDate.getFullYear()
-
-console.log(actualYear) */
 
 let priceFull = (userKm * 0.21).toFixed(2);;
 
@@ -20,4 +15,43 @@ if (userAge < 18) {
     priceSale = priceFull - (priceFull * 0.4).toFixed(2);
     console.log (priceSale)
 
-} else console.log (priceFull)
+} else console.log (priceFull) */
+
+const searchBtn = document.querySelector('.search');
+const deleteBtn = document.querySelector('.delete');
+
+searchBtn.addEventListener("click", function () {
+    //nome preso e messo in output
+    const fullName = document.querySelector('.nome-cognome');
+    const nomeCognome = document.querySelector('.outName');
+    nomeCognome.innerHTML = fullName.value;
+    //km presi e moltiplicati in base all eta
+    const inKm = document.querySelector('.distance');
+    const noSaleKm = parseInt(inKm.value) * 0.21;
+    const saleEl = document.getElementById('sale-option');
+    const sale = saleEl.value;
+    let priceWithSale;
+
+    if(fullName.value == ""){
+        alert("signore/a il nomeeeeeeeeeee!")
+    }
+
+    if(inKm.value == ""){
+        alert("inserisci un numero di km ktm!")
+    }
+
+    if (sale == 'under18') {
+        priceWithSale = noSaleKm - (noSaleKm * 0.2)
+    }
+    else if (sale == "over64") {
+        priceWithSale = noSaleKm - (noSaleKm * 0.4)
+    }
+    else {
+        priceWithSale = noSaleKm
+    }
+
+    console.log(priceWithSale);
+
+
+
+});
